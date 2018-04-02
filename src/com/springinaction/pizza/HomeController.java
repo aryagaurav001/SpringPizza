5e622b9c078544735a818b1c8a2a261728c66925
Gaurav Arya <aryagaurav_dce_cs@yahoo.co.in>
2018-04-03 01:09:47 +0530

    logging commmand removed

--- src/com/springinaction/pizza/HomeController.java
+++ src/com/springinaction/pizza/HomeController.java
@@ -12,7 +12,6 @@ public class HomeController {
 
 	@RequestMapping("/")
 	public String redirectToFlow() {
-		LOGGER.warn("Inside HomeController:redirectToFlow() method.");
 		return "redirect:/pizza";
 	}
 
