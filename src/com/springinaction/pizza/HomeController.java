782cb24dd1eb0f1f545a59b6d9ba045bbb09c91c
Gaurav Arya <aryagaurav_dce_cs@yahoo.co.in>
2018-04-17 20:55:20 +0530

    Loggers removed from home controller

--- src/com/springinaction/pizza/HomeController.java
+++ src/com/springinaction/pizza/HomeController.java
@@ -1,18 +1,13 @@
 package com.springinaction.pizza;
 
-import static org.apache.log4j.Logger.getLogger;
-
-import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-	private static final Logger LOGGER = getLogger(HomeController.class);
 
 	@RequestMapping("/")
 	public String redirectToFlow() {
 		return "redirect:/pizza";
 	}
-
 }
